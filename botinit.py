0c50d2b96746bc2d23abc5db8b29971a8ca077e8
Dylan Moore <hosnappp@gmail.com>
2015-07-05 09:37:48 -0400

    Update botinit.py
    Structural changes and much more customizable.

--- botinit.py
+++ botinit.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
  
-import tweepy, time, sys
+import tweepy, time, sys, os
+
+curdir = os.path.dirname(os.path.realpath(__file__))
+
+with open(curdir + '\OAUTH.txt', 'r') as f:
+    words = f.readlines()
+    data = [w.replace('\n', '') for w in words]
  
 # This is initialization for Twitter OAUTH
-CONSUMER_KEY = ''#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = ''#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = ''#keep the quotes, replace this with your access token
-ACCESS_SECRET = ''#keep the quotes, replace this with your access token secret
+CONSUMER_KEY = data[0]#keep the quotes, replace this with your consumer key
+CONSUMER_SECRET = data[1]#keep the quotes, replace this with your consumer secret key
+ACCESS_KEY = data[2]#keep the quotes, replace this with your access token
+ACCESS_SECRET = data[3]#keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+f.close()
 
 # This is the tweeting module
 def tweeter():
@@ -30,7 +38,7 @@ def favoriter():
 # This is the list of favorites and the option to remove those that aren't favoriting back.
 def list():
     n = 0
-    for user in tweepy.Cursor(api.followers, screen_name="The_LazySC").items():
+    for user in tweepy.Cursor(api.followers, screen_name=user_handle).items():
         n += 1
         print user.screen_name
         int(n)
@@ -38,39 +46,46 @@ def list():
     time.sleep(1)
     # Mutual destruction
     isitmutual = raw_input('Do you want to unfollow those who do not follow you currently? [Y/N]: ')
+
     if (isitmutual == 'y' or 'Y'):
-            followers = api.followers_ids("The_LazySC")
-            friends = api.friends_ids("The_LazySC")
+            followers = api.followers_ids(user_handle)
+            friends = api.friends_ids(user_handle)
             for f in friends:
                 if f not in followers:
                     print "Unfollow {0}?".format(api.get_user(f).screen_name)
                     if raw_input("Y/N?") == 'y' or 'Y':
-                        api.destroy_friendship(f)
+                        api.destroy_friendship(f)    
         
 # This is the menu
-ans=True
-while ans:
-    print("""
-    1. Post status.
-    2. Favoriting..
-    3. Hashtag/Mention follower.
-    4. List followers/unfollow.
-    5. Exit operation.
-    """)
-    ans=raw_input("What would you like to do? [1-5]: ")
-    if ans=="1":
-      print("\n Post status.")
-      tweeter()
-    elif ans=="2":
-      print("\n Turn favoriting on.")
-      favoriter()
-    elif ans=="3":
-      print("\n Turn hashtag follow on.")
-    elif ans=="4":
-      list()
-    elif ans=="5":
-        print("\n Exiting...")
-        ans = None
-    else:
-       print("\n Not a bot command.")
+def menu():
+    ans=True
+    while ans:
+        print("""
+        1. Post status.
+        2. Favoriting.
+        3. Hashtag/Mention follower.
+        4. List followers/unfollow.
+        5. Exit operation.
+        """)
+        ans=raw_input("What would you like to do? [1-5]: ")
+        if ans=="1":
+            print("\n Post status.")
+            tweeter()
+        elif ans=="2":
+            print("\n Turn favoriting on.")
+            favoriter()
+        elif ans=="3":
+            print("\n Turn hashtag follow on.")
+        elif ans=="4":
+            list()
+        elif ans=="5":
+            print("\n Exiting...")
+            ans = None
+        else:
+            print("\n Not a bot command.")
 
+user_handle = raw_input('What is your Twitter handle?\n')
+if len(user_handle) < 1:
+    print('Invalid username. Aborting...')
+else:
+    menu()
