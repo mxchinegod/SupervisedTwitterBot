97f8698356f0688e7ac0777cc4c82210c981c6e0
Dylan Moore <hosnappp@gmail.com>
2015-07-07 10:57:02 -0400

    Update botinit.py
    got it working despite unappreciated features

--- botinit.py
+++ botinit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
  
-import tweepy, time, os, repeater
+import tweepy, time, os, sys, random
+from twitter import TwitterHTTPError
 
 # This is initialization for Twitter OAUTH
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'OAUTH.txt')) as f:
     words = f.readlines()
-    data = [w.replace('\n', '') for w in words]
+    data = [w.replace('\n','') for w in words]
 
 CONSUMER_KEY = data[0]
 CONSUMER_SECRET = data[1]
@@ -18,22 +19,51 @@ api = tweepy.API(auth)
 
 f.close()
 
+    
+def __init__(self):
+        self.TWITTER_CONNECTION = None
+        random.seed()
+    
 # This is the tweeting module
 def tweeter():
-    tweet = raw_input('What would you like to tweet?\n')
-    if len(tweet) > 140:
-        print('It\'s over 140 characters...')
-    else:
-        print('\n Tweet live.')
-        api.update_status(status=tweet)
-        time.sleep(.5)
-  
-# This is the service for a favoriting job that occurs 2 times a day.
-def favoriter():
-    favstr = raw_input('Favorite based on which hashtag or mention?\n')
-    
-
-# This is the list of favorites and the option to remove those that aren't favoriting back.
+       tweet = raw_input('What would you like to tweet?\n')
+       if len(tweet) > 140:
+           print('It\'s over 140 characters...')
+       else:
+           print('\n Tweet live.')
+           api.update_status(status=tweet)
+           time.sleep(.5)
+   
+   # This is the tweet search module
+def search_tweets(self, phrase, count=100, result_type="recent"):
+   
+           return self.TWITTER_CONNECTION.search.tweets(q=phrase, result_type=result_type, count=count)
+           
+# This is the service for a favoriting job
+def favoriter(self, phrase, count=100, result_type="recent"):
+           
+       result = self.search_tweets(phrase, count, result_type)
+       
+       for tweet in result["statuses"]:
+           try:
+               # don't favorite your own tweets
+               if tweet["user"]["screen_name"] == user_handle:
+                   continue
+                   
+               result = self.TWITTER_CONNECTION.favorites.create(_id=tweet["id"])
+               print("Favorited: %s" % (result["text"].encode("utf-8")), file==sys.stdout)
+               
+           # when you have already favorited a tweet, this error is thrown
+           except TwitterHTTPError as api_error:
+               # quit on rate limit errors
+               if "rate limit" in str(api_error).lower():
+                   print("You have been rate limited. "
+                       "Wait a while before running the bot again.", file==sys.stderr)
+                   return
+                   
+               if "you have already favorited this status" not in str(api_error).lower():
+                   print("Error: %s" % (str(api_error)), file==sys.stderr)
+   # This is the list of favorites and the option to remove those that aren't favoriting back.
 def list():
     n = 0
     for user in tweepy.Cursor(api.followers, screen_name=user_handle).items():
@@ -57,36 +87,38 @@ def list():
         menu()
 # This is the menu
 def menu():
-    ans=True
-    while ans:
-        print("""
-        1. Post status.
-        2. Favoriting.
-        3. Hashtag follower.
-        4. List followers/unfollow.
-        5. Basic mention repeater.
-        6. Exit operation.
-        """)
-        ans=raw_input("What would you like to do? [1-6]: ")
-        if ans=="1":
-            print("\n Post status.")
-            tweeter()
-        elif ans=="2":
-            print("\n Turn favoriting on.")
-            favoriter()
-        elif ans=="3":
-            print("\n Turn hashtag follow on.")
-        elif ans=="4":
-            list()
-        elif ans=="5":
-            # This part needs to run repeater.py
-            menu()
-        elif ans=="6":
-            print("\n Exiting...")
-            ans = None
-        else:
-            print("\n Not a bot command.")
-
+     ans=True
+     while ans:
+            print("""
+            1. Post status.
+            2. Favoriting.
+            3. Hashtag follower.
+            4. List followers/unfollow.
+            5. Basic mention repeater.
+            6. Exit operation.
+            """)
+            ans=raw_input("What would you like to do? [1-6]: ")
+            if ans=="1":
+                print("\n Post status.")
+                tweeter()
+            elif ans=="2":
+                print("\n Turn favoriting on.")
+                hashtag=raw_input('\n Hashtag? :')
+                # This part needs to be programmed.
+                menu()
+            elif ans=="3":
+                print("\n Turn hashtag follow on.")
+            elif ans=="4":
+                list()
+            elif ans=="5":
+                # This part needs to run repeater.py
+                menu()
+            elif ans=="6":
+                print("\n Exiting...")
+                ans = None
+            else:
+                print("\n Not a bot command.")
+    
 user_handle = raw_input('What is your Twitter handle?\n')
 if len(user_handle) < 1:
     print(' Invalid username. Aborting...')
