0dd203595bc100badf1dabead74ede41678b4e6f
Dylan Moore <hosnappp@gmail.com>
2015-08-01 15:21:33 -0400

    Update to version 1.0.4 with algorithms
    Twitter mining utility completely expands the purpose.

--- botinit.py
+++ botinit.py
@@ -7,6 +7,8 @@ import time
 import random
 import mysql.connector
 from mysql.connector import errorcode
+import json
+import matplotlib.pyplot as plt
 
 # TWITTER BOT
 class TwitterBot(object):
@@ -94,8 +96,7 @@ class TwitterBot(object):
         if (time.time() - os.path.getmtime(self.BOT_CONFIG["FOLLOWS_FILE"]) > 86400 or
                 time.time() - os.path.getmtime(self.BOT_CONFIG["FOLLOWERS_FILE"]) > 86400):
             print("Warning: Your Twitter follower sync files are more than a day old. "
-                  "It is highly recommended that you sync them by calling sync_follows() "
-                  "before continuing.", file=sys.stderr)
+                  "Run a command to sync the followers list.", file=sys.stderr)
 
         # create an authorized connection to the Twitter API
         self.TWITTER_CONNECTION = Twitter(auth=OAuth(self.BOT_CONFIG["OAUTH_TOKEN"],
@@ -419,6 +420,111 @@ class TwitterBot(object):
     def send_tweet(self):
         message = raw_input("     What is your tweet?: ")
         return self.TWITTER_CONNECTION.statuses.update(status=message)
+    
+    def mine(self):
+        WORLD_WOE_ID = 1
+        US_WOE_ID = 23424977
+        q = raw_input("     What hashtag or phrase would you like to run algorithms on?: ")
+        count = 100000
+        # See https://dev.twitter.com/docs/api/1.1/get/search/tweets
+        search_results = self.TWITTER_CONNECTION.search.tweets(q=q, count=count)
+        statuses = search_results['statuses']
+        # Iterate through 5 more batches of results by following the cursor
+        for _ in range(100):
+            print('Length of statuses'), len(statuses)
+            try:
+                next_results = search_results['search_metadata']['next_results']
+            except KeyError, e: # No more results when next_results doesn't exist
+                break
+        # Create a dictionary from next_results, which has the following form:
+        # ?max_id=313519052523986943&q=NCAA&include_entities=1
+        kwargs = dict([ kv.split('=') for kv in next_results[1:].split("&") ])
+        search_results = self.TWITTER_CONNECTION.search.tweets(**kwargs)
+        statuses += search_results['statuses']
+        # Show one sample search result by slicing the list...
+        print(json.dumps(statuses[0], indent=1))
+        status_texts = [ status['text']
+        for status in statuses ]
+        screen_names = [ user_mention['screen_name']
+        for status in statuses
+        for user_mention in status['entities']['user_mentions'] ]
+        hashtags = [ hashtag['text']
+        for status in statuses
+        for hashtag in status['entities']['hashtags'] ]
+        # Compute a collection of all words from all tweets
+        words = [ w
+        for t in status_texts
+        for w in t.split() ]
+        # Explore the first 5 items for each...
+        print(json.dumps(status_texts[0:100], indent=1))
+        print(json.dumps(screen_names[0:100], indent=1))
+        print(json.dumps(hashtags[0:100], indent=1))
+        print(json.dumps(words[0:100], indent=1))
+        
+        from collections import Counter
+        for item in [words, screen_names, hashtags]:
+            c = Counter(item)
+            print(c.most_common()[:10]) # top 10
+            print
+        from prettytable import PrettyTable
+        for label, data in (('Word', words),
+                ('Screen Name', screen_names),
+                ('Hashtag', hashtags)):
+            pt = PrettyTable(field_names=[label, 'Count'])
+            c = Counter(data)
+            [ pt.add_row(kv) for kv in c.most_common()[:10] ]
+            pt.align[label], pt.align['Count'] = 'l', 'r' # Set column alignment
+            print(pt)
+        retweets = [
+        # Store out a tuple of these three values ...
+        (status['retweet_count'],
+        status['retweeted_status']['user']['screen_name'],
+        status['text'])
+        # ... for each status ...
+        for status in statuses
+        # ... so long as the status meets this condition.
+        if status.has_key('retweeted_status')
+        ]
+        # Slice off the first 5 from the sorted results and display each item in the tuple
+        pt = PrettyTable(field_names=['Count', 'Screen Name', 'Text'])
+        [ pt.add_row(row) for row in sorted(retweets, reverse=True)[:5] ]
+        pt.max_width['Text'] = 50
+        pt.align= 'l'
+        print(pt)
+        
+        word_counts = sorted(Counter(words).values(), reverse=True)
+        
+        plt.loglog(word_counts)
+        plt.ylabel("Freq")
+        plt.xlabel("Word Rank")
+        plt.show()
+        for label, data in (('Words', words),
+                            ('Screen Names', screen_names),
+                            ('Hashtags', hashtags)):
+                            
+            # Build a frequency map for each set of data
+            # and plot the values
+            c = Counter(data)
+            plt.hist(c.values())
+            
+            # Add a title and y-label ...
+            plt.title(label)
+            plt.ylabel("Number of items in bin")
+            plt.xlabel("Bins (number of times an item appeared)")
+            
+            # ... and display as a new figure
+            plt.figure()
+        
+        # Using underscores while unpacking values in
+        # a tuple is idiomatic for discarding them
+        counts = [count for count, _, _ in retweets]
+        
+        plt.hist(counts)
+        plt.title("Retweets")
+        plt.xlabel('Bins (number of times retweeted)')
+        plt.ylabel('Number of tweets in bin')
+        plt.show()
+        print(counts)
         
 # MENU CLASS  
 class menu():
@@ -431,9 +537,10 @@ class menu():
                 3. Hashtag follower.
                 4. List followers/unfollow.
                 5. Basic mention repeater.
-                6. Exit operation.
+                6. Data Mine.
+                7. Exit operation.
                 """)
-                ans=raw_input("     What would you like to do? [1-6]: ")
+                ans=raw_input("     What would you like to do? [1-7]: ")
                 if ans=="1":
                     print("\n Post status.")
                     my_bot = TwitterBot()
@@ -467,13 +574,16 @@ class menu():
                     bot = TwitterBot()
                     bot.auto_rt(hashphrase,int(rtcount))
                 elif ans=="6":
+                    bot = TwitterBot()
+                    bot.mine()
+                elif ans=="7":
                     print("\n Exiting...")
                     sys.exit()
                 else:
                     print("\n Not a bot command.")
                     
 # DRM & INITIALIZATION BLOCK
-print("\n     Snakey Login (v. 1.0.3)")
+print("\n     Snakey Login (v. 1.0.4)")
 username = raw_input("     User: ")
 password = raw_input("     Password: ")
 drm = mysql.connector.connect(user='', password='',
